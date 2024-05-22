e242b6e0f94b965357ef2b294d26f8e6984dae69
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-22 16:02:03 +0300

    Added Email Info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_uq00gwq', // paste your ServiceID here (you'll get one when your service is created).
+        'template_fznqiiq', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Mohamed diaa', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'mdiaan442@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        '2i4W_kTXIQi7B99KN' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
